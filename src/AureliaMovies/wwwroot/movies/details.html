9382b6cbab5f19dd7f77296bb9e3b16974aafcc6
K. Scott Allen <scott@OdeToCode.com>
2015-04-25 23:37:53 -0400

    rc tweaks

---
+++ src/AureliaMovies/wwwroot/movies/details.html
@@ -0,0 +1,3 @@
+﻿<template>
+
+</template>
---
+++ src/AureliaMovies/wwwroot/movies/movieData.js
@@ -0,0 +1,18 @@
+﻿import {Inject} from "aurelia-framework";
+import {HttpClient} from "aurelia-http-client";
+
+@inject(HttpClient)
+export class MovieData {
+    
+    constructor(httpClient) {
+        this.http = httpClient;
+    }
+
+    getAll() {
+        return this.http.get("/api/movies")
+                        .then(message => {                         
+                            this.movies = JSON.parse(message.response);
+                            return this.movies;
+                        });
+    }
+}
