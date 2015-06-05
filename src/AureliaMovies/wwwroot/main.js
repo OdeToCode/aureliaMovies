75308b58af25ab76d570f58ba9c22cf667c41840
K. Scott Allen <scott@OdeToCode.com>
2015-06-05 15:52:45 +0000

    globalize navbar

--- src/AureliaMovies/wwwroot/main.js
+++ src/AureliaMovies/wwwroot/main.js
@@ -2,7 +2,8 @@
     aurelia.use
            .standardConfiguration()
            .developmentLogging()
-           .plugin("aurelia-validation");
+           .plugin("aurelia-validation")
+           .plugin("./resources/index");
 
     aurelia.start().then(a => a.setRoot("app", document.body));
 };
