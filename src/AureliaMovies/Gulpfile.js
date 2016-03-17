186f2d0915586bea17897d36610bdbb5058cb108
K. Scott Allen <scott@OdeToCode.com>
2016-03-17 15:29:15 -0400

    updated with aurelia-bundler

--- src/AureliaMovies/Gulpfile.js
+++ src/AureliaMovies/Gulpfile.js
@@ -1,25 +1,64 @@
 ﻿var gulp = require("gulp");
 var watch = require("gulp-watch");
-var exec = require('child_process').exec;
-var uglify = require("gulp-uglify");
+var bundler = require("aurelia-bundler");
+var del = require("del");
 
-gulp.task("bundle", function (cb) {
-    exec("aurelia bundle", function (err) {
-        cb(err);
-    });
+var config = {
+    force: true,
+    baseURL: "./wwwroot",        
+    configPath: "./wwwroot/config.js", 
+    bundles: {
+        "app/app-bundle": {
+            includes: [
+                "[*.js]",
+                "*.html!text",
+                "*.css!text"
+            ],
+            options: {
+                inject: true,
+                minify: true
+            }
+        },
+        "app/vendor-build": {
+            includes: [
+                "aurelia-bootstrapper",
+                "aurelia-framework",
+                "aurelia-loader-default",
+                "aurelia-logging-console",
+                "aurelia-router",
+                "aurelia-templating",
+                "aurelia-templating-binding",
+                "aurelia-templating-router",
+                "aurelia-templating-resources",
+                "aurelia-history-browser",
+                "aurelia-http-client",
+                "aurelia-validation"
+            ],
+            options: {
+                inject: true,
+                minify: true
+            }
+        }
+    }
+};
+
+gulp.task("clean", () => {
+    return del(["wwwroot/app-bundle.js"]);
 });
 
-gulp.task("uglify", ["bundle"], function () {
-    return gulp.src("wwwroot/appbundle.js")
-               .pipe(uglify())
-               .pipe(gulp.dest("wwwroot/"));
+gulp.task('bundle', ['unbundle'], function () {
+    return bundler.bundle(config);
+});
+
+gulp.task('unbundle', function () {
+    return bundler.unbundle(config);
 });
 
 gulp.task("watch", function () {
     gulp.watch(["wwwroot/**/*.js", "wwwroot/**/*.html",
-                "!wwwroot/*bundle*"], ["uglify"]);
+                "!wwwroot/*bundle*"], ["bundle"]);
 });
 
-gulp.task("default", ["uglify"]);
+gulp.task("default", ["bundle"]);
 
 
--- src/AureliaMovies/wwwroot/app.js
+++ src/AureliaMovies/wwwroot/app.js
@@ -5,17 +5,10 @@
  
         config.title = "Movies";
         config.map([
-            {
-                route: "", name: 'home', moduleId: "movies/list", 
-                title:"List", nav:true, settings: {
-                    icon: "home"
-                }
-                
-
+            { route: "", name: 'home', moduleId: "movies/list", 
+              title:"List", nav:true, settings: { icon: "home" }                
             },
             { route: "about", moduleId: "about/about", title: "About", nav:true },
-
-
             { route: "details/:id", name:"details", moduleId: "movies/details" },
             { route: "edit/:id", name:"edit", moduleId: "movies/edit" },
             { route: "create", name:"create", moduleId:"movies/edit" }
